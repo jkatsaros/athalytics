c6b354d76dbf90f55c3ce694789d8af70cbbce91
Jason Katsaros <40249933+jkatsaros@users.noreply.github.com>
2023-10-09 19:44:18 -0400

    feat: create a wrapper around the ffanalytics library

---
+++ ffanalytics.R
@@ -0,0 +1,41 @@
+if (!require(remotes))
+	install.packages("remotes", repos = "http://cran.us.r-project.org")
+	
+remotes::install_github("FantasyFootballAnalytics/ffanalytics")
+
+possible_season_sources <- c("CBS", "ESPN", "FantasyPros", "FantasySharks", "FFToday", "NumberFire", "FantasyFootballNerd", "NFL", "RTSports", "Walterfootball")
+possible_week_sources <- c("CBS", "ESPN", "FantasyPros", "FantasySharks", "FFToday", "FleaFlicker", "NumberFire", "FantasyFootballNerd", "NFL")
+possible_position_sources <- c("QB", "RB", "WR", "TE", "DST")
+
+scrape_ffanalytics <- function(season, week, sources, positions) {
+	ffanalytics::scrape_data(season = season, week = week, src = sources, pos = position)
+}
+
+projections_ffanaytics <- function(scrape_data, include_ecr = FALSE, include_adp = FALSE, include_aav = FALSE, include_uncertainty = FALSE) {
+	projections <- ffanalytics::projects_table(scrape_data)
+	
+	if (include_ecr) {
+		projections <- projections %>%
+			ffanalytics::add_ecr()
+	}
+	
+	if (include_adp) {
+		projections <- projections %>%
+			ffanalytics::add_adp()
+	}
+	
+	if (include_aav) {
+		projections <- projections %>%
+			ffanalytics::add_aav()
+	}
+	
+	if (include_uncertainty) {
+		projections <- projections %>%
+			ffanalytics::add_uncertainty()
+	}
+	
+	projections <- projections %>%
+		ffanalytics::add_player_info()
+		
+	projections
+}
