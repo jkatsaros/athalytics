a1486a68c0521f870422f1f888674133305c7ed3
Jason Katsaros <40249933+jkatsaros@users.noreply.github.com>
2023-10-09 20:10:29 -0400

    chore: adjust tab width

--- ffanalytics.R
+++ ffanalytics.R
@@ -1,7 +1,7 @@
 if (!require(remotes))
-    install.packages("remotes", repos = "http://cran.us.r-project.org")
+  install.packages("remotes", repos = "http://cran.us.r-project.org")
 if (!require(tidyverse))
-    install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+  install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 
 library(remotes)
 library(tidyverse)
@@ -9,81 +9,81 @@ library(tidyverse)
 remotes::install_github("FantasyFootballAnalytics/ffanalytics")
 
 possible_season_sources <- c(
-    "CBS",
-    "ESPN",
-    "FantasyPros",
-    "FantasySharks",
-    "FFToday",
-    "NumberFire",
-    "FantasyFootballNerd",
-    "NFL",
-    "RTSports",
-    "Walterfootball"
+  "CBS",
+  "ESPN",
+  "FantasyPros",
+  "FantasySharks",
+  "FFToday",
+  "NumberFire",
+  "FantasyFootballNerd",
+  "NFL",
+  "RTSports",
+  "Walterfootball"
 )
 possible_week_sources <- c(
-    "CBS",
-    "ESPN",
-    "FantasyPros",
-    "FantasySharks",
-    "FFToday",
-    "FleaFlicker",
-    "NumberFire",
-    "FantasyFootballNerd",
-    "NFL"
+  "CBS",
+  "ESPN",
+  "FantasyPros",
+  "FantasySharks",
+  "FFToday",
+  "FleaFlicker",
+  "NumberFire",
+  "FantasyFootballNerd",
+  "NFL"
 )
 possible_position_sources <- c(
-    "QB",
-    "RB",
-    "WR",
-    "TE",
-    "DST"
+  "QB",
+  "RB",
+  "WR",
+  "TE",
+  "DST"
 )
 
 scrape_ffanalytics <- function(
-    sources,
-    positions,
-    season = NULL,
-    week = NULL
+  sources,
+  positions,
+  season = NULL,
+  week = NULL
 ) {
-    ffanalytics::scrape_data(
-        src = sources,
-        pos = position,
-        season = season,
-        week = week
-    )
+  ffanalytics::scrape_data(
+    src = sources,
+    pos = position,
+    season = season,
+    week = week
+  )
 }
 
 projections_ffanaytics <- function(
-    scrape_data,
-    include_ecr = FALSE,
-    include_adp = FALSE,
-    include_aav = FALSE,
-    include_uncertainty = FALSE
+  scrape_data,
+  include_ecr = FALSE,
+  include_adp = FALSE,
+  include_aav = FALSE,
+  include_uncertainty = FALSE
 ) {
-    projections <- ffanalytics::projects_table(scrape_data)
+  projections <- ffanalytics::projects_table(scrape_data)
 
-    if (include_ecr) {
-        projections <- projections %>%
-            ffanalytics::add_ecr()
-    }
-
-    if (include_adp) {
-        projections <- projections %>%
-            ffanalytics::add_adp()
-    }
-
-    if (include_aav) {
-        projections <- projections %>%
-            ffanalytics::add_aav()
-    }
-
-    if (include_uncertainty) {
-        projections <- projections %>%
-            ffanalytics::add_uncertainty()
-    }
-
+  if (include_ecr) {
     projections <- projections %>%
-        ffanalytics::add_player_info()
+      ffanalytics::add_ecr()
+  }
 
-    projections
+  if (include_adp) {
+    projections <- projections %>%
+      ffanalytics::add_adp()
+  }
+
+  if (include_aav) {
+    projections <- projections %>%
+      ffanalytics::add_aav()
+  }
+
+  if (include_uncertainty) {
+    projections <- projections %>%
+      ffanalytics::add_uncertainty()
+  }
+
+  projections <- projections %>%
+    ffanalytics::add_player_info()
+
+  projections
 }
