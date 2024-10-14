b82404ff974aa033d8d506cfc6398e1b641fb1dd
Jason Katsaros <40249933+jkatsaros@users.noreply.github.com>
2024-10-14 19:15:11 -0400

    feat: add overall health check

--- R/api.R
+++ R/api.R
@@ -6,5 +6,9 @@ library(plumber)
 source("football-api.R")
 
 pr() %>%
+  pr_get(
+    path = "/",
+    responses = list("200" = list(description = "Health check."))
+  ) %>%
   pr_mount("/football", plumb("./football-api.R")) %>%
   pr_run()
